630202e301bd79dac012f88ed4981d9b1c48877a
brapen57 <144976865+brapen57@users.noreply.github.com>
2024-01-07 05:10:57 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.proyectoultimounidad2',
+  appName: 'proyectoultimounidad2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
